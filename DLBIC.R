5f3399a472a808b9ef8a6f07028f62402602c18b
therealkundanpatil <kp28021996@gmail.com>
2020-08-13 22:04:31 -0400

    -Performed Normalization and generated plots

--- DLBIC.R
+++ DLBIC.R
@@ -45,4 +45,31 @@ head(train_labels)
 head(test_labels)
 
 
+#Data Preprocessing(Normalization)
+train_images <- train_images / 255
+test_images <- test_images / 255
 
+image_1 <- as.data.frame(train_images[1, , ])
+colnames(image_1) <- seq_len(ncol(image_1))
+image_1$y <- seq_len(nrow(image_1))
+image_1 <- gather(image_1, "x", "value" , -y) 
+image_1$x <- as.integer(image_1$x)
+
+ggplot( image_1 , aes( x = x, y = y, fill = value )) +
+  geom_tile() +
+  scale_fill_gradient( low = "white", high = "black", na.value = NA ) +
+  scale_y_reverse() +
+  theme_minimal() +
+  theme(panel.grid = element_blank()) + 
+  theme(aspect.ratio = 1) + 
+  xlab("") + 
+  ylab("")
+
+par(mfcol = c( 5, 5 ))
+par(mar = c( 0, 0, 1.5, 0), xaxs = "i", yaxs = "i")
+
+for (i in 1:25){
+  img <- train_images[i, ,]
+  img <- t(apply(img, 2, rev))
+  image( 1:28, 1:28, img, col = gray((0:255) / 255), xaxt = "n", yaxt = "n", main = paste(class_names[train_labels[i]+1]))
+}
