2b091bd14abfe6f8a3e0671c059d2d3f74ced44d
therealkundanpatil <kp28021996@gmail.com>
2020-08-13 21:15:10 -0400

    -Selected inbuilt dataset

--- DLBIC.R
+++ DLBIC.R
@@ -11,3 +11,20 @@ library(tidyverse)
 library(ggplot2)
 
 
+#Import desired dataset
+fashion_mnist <- dataset_fashion_mnist()
+fashion_mnist$train
+c(train_images, train_labels) %<-% fashion_mnist$train
+c(test_images, test_labels) %<-% fashion_mnist$test
+class_names <- c(
+  "T-shirt/Top",
+  "Trouser",
+  "Pullover",
+  "Dress",
+  "Coat",
+  "Sandal",
+  "Shirt",
+  "Sneaker",
+  "Bag",
+  "AnkleBoot")
+
